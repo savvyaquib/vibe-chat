ecf60b6f6badbb5c9ceff8cf022e82dbb58f5e08
savvyaquib <itsmohammadaquib@gmail.com>
2026-05-09 07:23:00 +0530

    feat: add auth route and controller

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import authRoutes from "./routes/auth.route.js";
 
 dotenv.config();
 
@@ -8,12 +9,17 @@ const app = express();
 app.use(cors());
 app.use(express.json()); 
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5500;
 
 app.get("/", (req, res) => {
   res.send("Hello, World!");
 });
 
+// routes
+app.use("/api/v1/auth", authRoutes);
+
+
+
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
