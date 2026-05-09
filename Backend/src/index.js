69690167a7013137b1b505a28665155fa2f12ada
savvyaquib <itsmohammadaquib@gmail.com>
2026-05-09 07:34:27 +0530

    feat: implement MongoDB connection logic

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -2,12 +2,13 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js";
+import { connectDB } from "./lib/db.js";
 
 dotenv.config();
 
 const app = express();
 app.use(cors());
-app.use(express.json()); 
+app.use(express.json());
 
 const PORT = process.env.PORT || 5500;
 
@@ -22,4 +23,5 @@ app.use("/api/v1/auth", authRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
+  connectDB();
 });
