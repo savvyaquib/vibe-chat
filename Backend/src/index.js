7fccb6f8b44f176e65b5c8240ce5ff7c53a187d6
savvyaquib <itsmohammadaquib@gmail.com>
2026-05-10 21:54:10 +0530

    feat: add user.model.js

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -7,6 +7,7 @@ import { connectDB } from "./lib/db.js";
 dotenv.config();
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 
---
+++ Backend/src/models/user.model.js
@@ -0,0 +1,28 @@
+import e from "express";
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        lowercase: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 6,
+    },
+    profilePic: {
+        type: String,
+        default: "",
+    }
+}, { timestamps: true });
+
+const User = mongoose.model("User", userSchema);
+export default User 
