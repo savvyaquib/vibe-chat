26418b42fb6182783f8cc3856243db4a38ac0a6c
savvyaquib <itsmohammadaquib@gmail.com>
2026-05-14 16:53:21 +0530

    fix: correct import path for auth middleware in message routes

--- Backend/src/routes/message.route.js
+++ Backend/src/routes/message.route.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
-import { verifyJWT } from "../middlewares/auth.middleware";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
+import { getMessage, getUsers } from "../controllers/message.controller.js";
 
 const router = Router()
 
