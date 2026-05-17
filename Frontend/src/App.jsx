9c5b93ea34d9b065c46f72cae5ad0d5ce5dae7a5
savvyaquib <itsmohammadaquib@gmail.com>
2026-05-17 23:02:34 +0530

    feat: add essential pages

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,9 +1,25 @@
+import Navbar from "./components/Navbar.jsx";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import Singup from "./pages/Singup.jsx";
+import Login from "./pages/Login.jsx";
+import Profile from "./pages/Profile.jsx";
+import Settings from "./pages/Settings.jsx";
+
 function App() {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-slate-950 text-white text-3xl font-bold">
-      Welcome to Vibe Chat
+    <div>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signup" element={<Singup />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/settings" element={<Settings />} />
+      </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -1,3 +1,4 @@
+import dasyui from "daisyui";
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
@@ -7,5 +8,5 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [dasyui],
 }
