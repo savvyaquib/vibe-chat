7a465134de732924c9530ea9ee184ab5b66168c5
savvyaquib <itsmohammadaquib@gmail.com>
2026-05-20 00:25:41 +0530

    feat: implement signup form with validation and layout

--- Frontend/src/pages/Singup.jsx
+++ Frontend/src/pages/Singup.jsx
@@ -1,9 +1,55 @@
-const Singup = () => {
-  return (
-    <div>
-      Singup
-    </div>
-  )
-}
+import { MessageSquare } from "lucide-react";
 
-export default Singup
+const Singup = () => {
+  const [showPassword, setShowPassword] = useState(false);
+  const [formData, setFormData] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+
+  const { signup, isSigningUp } = useAuthStore();
+  const validateForm = () => {
+    const { username, email, password } = formData;
+    if (!username || !email || !password) {
+      alert("Please fill in all fields.");
+      return false;
+    }
+    return true;
+  };
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!validateForm()) return;
+
+    await signup(formData);
+  };
+
+  return (
+    <div className="min-h-screen grid lg:grid-cols-2">
+      {/* left side */}
+      <div className="flex flex-col justify-center items-center p-6 sm:12">
+        <div className="w-full max-w-md space-y-8">
+          <div className="text-center mb-8">
+            <div className="flex flex-col items-center gap-2 group">
+              <div className="size-12 rounded-xl bg-primary/10 flex items-center justify-center group-hover:bg-primary/20 transition-colors">
+                <User className="size-6 text-primary" />
+                <MessageSquare className="size-6 text-primary" />
+              </div>
+              <h1 className="text-2xl font-bold mt-2">Create an account</h1>
+              <p className="text-base-content/60">
+                Get started with your free account
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Singup;
