83f4d21a103821d4d45ebc09f93b9566608777b7
savvyaquib <itsmohammadaquib@gmail.com>
2026-06-05 22:57:19 +0530

    feat: enhance socket connection handling and manage online users

--- Backend/src/lib/socket.js
+++ Backend/src/lib/socket.js
@@ -8,15 +8,33 @@ const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
         origin: ['http://localhost:5173', 'http://localhost:5174'],
+        credentials: true,
     }
-})
+});
+
+const onlineUsers = new Map();
 
 io.on('connection', (socket) => {
     console.log('a user connected', socket.id);
 
-    io.disconnectSockets((socket) => {
+    socket.on('join', (userId) => {
+        if (userId) {
+            onlineUsers.set(userId, socket.id);
+            io.emit('online_users', Array.from(onlineUsers.keys()));
+            console.log(`user joined ${userId} -> ${socket.id}`);
+        }
+    });
+
+    socket.on('disconnect', () => {
+        for (const [userId, sid] of onlineUsers.entries()) {
+            if (sid === socket.id) {
+                onlineUsers.delete(userId);
+                break;
+            }
+        }
+        io.emit('online_users', Array.from(onlineUsers.keys()));
         console.log('a user disconnected', socket.id);
     });
 });
 
-export { io, server, app };
+export { io, server, app, onlineUsers };
